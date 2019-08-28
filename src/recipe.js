e43ea055d7a8f8ac364981d04dd5f63ded37e6a6
riyadh <riyadh02220@gmail.com>
2019-08-28 20:21:24 +0200

    hide recipe when passing to another

--- src/recipe.js
+++ src/recipe.js
@@ -1,32 +1,26 @@
 import React from "react";
 import './App.css'
 
-/*
-window.addEventListener("click", ()=> {
-    document.querySelector(".ingridients").style.visibility = "hidden";
-})
-*/
-
 const showIngridients =(ev)=> {
-    document.querySelector("ul").style.visibility = "hidden";
-    ev.target.firstChild.style.visibility = "visible";
+    ev.target.parentElement.firstChild.style.visibility = "visible";
 }
-/*
+
 const hideReciepe =(ev)=> {
-    ev.target.firstChild.style.visibility = "hidden";
+    ev.target.parentElement.firstChild.style.visibility = "hidden";
   }
-*/
+
 const Recipe = (props)=> {
     return (
         <div className="recipe">
             <div className="recipeTitle">{props.title}</div>
             <div className="recipeCalories">{props.calories.toFixed(2)} Kcal</div>
-            <div onClick={showIngridients} className="recipeimage"  style={{backgroundImage: `url(${props.imageLink})`}}>
+            <div  className="recipeimage"  style={{backgroundImage: `url(${props.imageLink})`}}>
                 <ul className="ingridients">
                     {props.ingridients.map(ingredient=> (
                         <li className="ingridient">{ingredient.text}</li>
                     ))}
                 </ul>
+                <input type="button" onFocus={showIngridients} onBlur={hideReciepe} className="recipe-button"></input>
             </div>
         </div>
     )
